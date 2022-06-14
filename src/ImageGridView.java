90cfb67927196b091650e878e13d7fd0068cc998
mahdi8085 <mahdi8085@gmail.com>
2022-06-14 23:39:39 +0430

    large view completed

--- src/ImageGridView.java
+++ src/ImageGridView.java
@@ -4,6 +4,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.geometry.Insets;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 
@@ -12,12 +13,12 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 public class ImageGridView implements Initializable {
 
-    @FXML GridPane gridPane;
+    @FXML public ImageView largeImg;
+    @FXML public GridPane gridPane;
 
     private static HttpURLConnection connection;
 
@@ -41,9 +42,9 @@ public class ImageGridView implements Initializable {
                 try {
                     VBox imageClass = fxmlLoader.load();
                     ImageItem imageItem = fxmlLoader.getController();
-                    imageItem.setData(list.getList().get(i), j);
+                    imageItem.setData(list.getList().get(i), j, this);
 
-                    if (column == 3) {
+                    if (column == 2) {
                         column = 0;
                         row++;
                     }
@@ -62,7 +63,7 @@ public class ImageGridView implements Initializable {
 
         BufferedReader reader;
         String line;
-        StringBuffer responseContent = new StringBuffer();
+        StringBuilder responseContent = new StringBuilder();
 
         try {
             URL url = new URL("http://gandom.co/devTest/1/home");
--- src/ImageItem.java
+++ src/ImageItem.java
@@ -3,15 +3,29 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 
 public class ImageItem {
 
-    @FXML private ImageView img;
-    @FXML private Label label;
+    private ImageClass imageClass;
+    private int i;
+    private ImageGridView imageGridView;
 
-    public void setData(ImageClass images, int i) {
+    @FXML public ImageView img;
+    @FXML public Label label;
+
+    public void setData(ImageClass images, int i, ImageGridView imageGridView) {
+        this.imageClass = images;
+        this.i = i;
+        this.imageGridView = imageGridView;
         Image image = new Image("http://gandom.co/devTest/1/images/" + images.getImages().get(i));
         img.setImage(image);
         label.setText(images.getDescription());
+    }
+
+    @FXML
+    private void click(MouseEvent mouseEvent) {
+        Image image = new Image("http://gandom.co/devTest/1/images/" + imageClass.getImages().get(i));
+        imageGridView.largeImg.setImage(image);
     }
 }
